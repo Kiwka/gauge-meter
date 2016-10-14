131068a1bc45ff6c1f61ad3d64025d81d7978f55
Olena Sovyn <olenasovyn@gmail.com>
2016-10-14 21:01:01 +0200

    Read data from store + handle error

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
-import AppHeader from './components/header.js';
+import getData from './stores/gauge-store.js';
+import AppHeader from './components/header.jsx';
+import Error from './components/error.jsx';
+import GaugeMeter from './components/gauge-meter.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isError: false, data: {}};
+  }
+
+  componentDidMount() {
+    getData()
+      .then(res => {
+        if (res.error) {
+          this.setState({
+            isError: true,
+          })
+        } else {
+          this.setState({
+            data: res,
+            isError: false,
+          })
+        }
+      })
+      .catch(() => {
+        this.setState({
+          isError: true,
+        })
+      });
+  }
+
   render() {
     return (
       <div className="App">
         <AppHeader/>
-        <p>
-          Gauge meter will be here
-        </p>
+        {this.state.isError ? <Error /> : <GaugeMeter data={this.state.data}/> }
       </div>
     );
   }
---
+++ src/components/gauge-meter.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const GaugeMeter = () => <div>Gauge meter will be here</div>;
+
+export default GaugeMeter;
