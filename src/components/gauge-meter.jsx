ea7f77f2c48be1456a3b5be22bc68f94fcdce8a0
Olena Sovyn <olenasovyn@gmail.com>
2016-10-14 22:33:20 +0200

    Create background for gauge meter

--- src/components/gauge-meter.jsx
+++ src/components/gauge-meter.jsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const GaugeMeter = () => <div>Gauge meter will be here</div>;
+class GaugeMeter extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div className="holder">
+      <svg className="meter">
+        <clipPath id="cut-off-bottom">
+            <rect x="0" y="0" width="408" height="204" />
+        </clipPath>
+        <circle r="200" cx="204" cy="0" stroke="#000"
+        strokeWidth="5" strokeDasharray="640, 943" fill="#FA891E"
+        clipPath="url(#cut-off-bottom)" />
+      </svg>
+    </div>;
+  }
+};
+
+GaugeMeter.propTypes = {
+  data: React.PropTypes.object,
+};
 
 export default GaugeMeter;
